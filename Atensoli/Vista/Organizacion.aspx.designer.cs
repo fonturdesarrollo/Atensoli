bbb291f73a6a710a67f25d82260804e328647862
sjhernandez <samueljhernandezb@gmail.com>
2018-04-05 16:02:10 -0400

    correccion formulario organizacion validacion

--- Atensoli/Vista/Organizacion.aspx.designer.cs
+++ Atensoli/Vista/Organizacion.aspx.designer.cs
@@ -130,6 +130,15 @@ namespace Atensoli {
         protected global::System.Web.UI.WebControls.DropDownList ddlNieto;
         
         /// <summary>
+        /// txtTelefonoOrganizacion control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefonoOrganizacion;
+        
+        /// <summary>
         /// rqrValidaTelefonoOrg control.
         /// </summary>
         /// <remarks>
